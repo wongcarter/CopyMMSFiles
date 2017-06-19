5782970472a455cd11360172d399e68a83d7a9d8
Austin <austinwongcarter@gmail.com>
2017-06-18 23:01:25 -0700

    added Auth and Proxy
    Added Austhentication and proxy option.

--- CopyMMS2017Files.ps1
+++ CopyMMS2017Files.ps1
@@ -5,12 +5,23 @@
 #           http://www.sysadmintechnotes.com #
 # Edited:   Andrew Johnson                   #
 #           http://www.andrewj.net           #
+#           Austin WongCarter                #
+#           http://wongcarter.com            #
 #                                            #
 ##############################################
 
-$baseLocation = 'C:\Conferences\MMS'
+
+$proxy = 'http://yourproxy.com'
+$PSDefaultParameterValues = @{"Invoke-WebRequest:Proxy"="$proxy";"Invoke-WebRequest:ProxyUseDefaultCredentials"=$true}
+$baseLocation = 'C:\temp\MMS'
 Clear-Host
-$MMSDates='http://mms2017.sched.org/2017-05-14', 'http://mms2017.sched.org/2017-05-15', 'http://mms2017.sched.org/2017-05-16','http://mms2017.sched.org/2017-05-17','http://mms2017.sched.org/2017-05-18'
+$username = Read-Host -Prompt "Sched UserName" -AsSecureString
+$password = Read-Host -Prompt "Sched Password" -AsSecureString
+$postParams = @{username="$username";password="$password"; landing_conf='https://mms2017.sched.com'; login='Log In'}
+$login = Invoke-WebRequest -Uri 'https://mms2017.sched.com/login' -Method POST -Body $postParams -SessionVariable "mms"
+Remove-Variable -Name postParams -Force
+
+$MMSDates='http://mms2017.sched.com/2017-05-14', 'http://mms2017.sched/2017-05-15', 'http://mms2017.sched.com/2017-05-16','http://mms2017.sched.com/2017-05-17','http://mms2017.sched.com/2017-05-18'
 ForEach ($Date in $MMSDates)
 {
 $uri = $Date
@@ -24,7 +35,7 @@ $links | ForEach-Object {
         "Checking session '{0}' for downloads" -f $eventTitle
         $eventTitle = $eventTitle -replace "\W+", "_"
         
-        $uri = 'http://mms2017.sched.org'
+        $uri = 'http://mms2017.sched.com'
         $event = Invoke-WebRequest -Uri $($uri + $eventUrl)
         $eventLinks = $event.Links
 
@@ -39,7 +50,7 @@ $links | ForEach-Object {
                 if((Test-Path -Path $outputFilePath) -eq $false)
                 {
                     "...attempting to download '{0}'" -f $filename
-                    Invoke-WebRequest -Uri $eventFileUrl -OutFile $outputfilepath -WebSession $mms;$doDownload=$false;
+                    Invoke-WebRequest -Uri $eventFileUrl -OutFile $outputfilepath -WebSession $mms
                     Unblock-File $outputFilePath
                     $stopit = $true
                 }
